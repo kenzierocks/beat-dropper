6f7f1ff29d2f673dc066574e40a66b0cf9840d86
Kenzie Togami <ket1999@gmail.com>
2018-11-23 23:34:18 -0800

    Update gradle, IB, dependencies

--- src/main/java/net/octyl/beatdropper/BeatDrop.java
+++ src/main/java/net/octyl/beatdropper/BeatDrop.java
@@ -22,6 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package net.octyl.beatdropper;
 
 import java.io.IOException;
--- src/main/java/net/octyl/beatdropper/droppers/SampleSelectionUtils.java
+++ src/main/java/net/octyl/beatdropper/droppers/SampleSelectionUtils.java
@@ -22,6 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package net.octyl.beatdropper.droppers;
 
 import java.util.concurrent.TimeUnit;
